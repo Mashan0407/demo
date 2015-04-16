920c741214d2b9959666e3ff111a687f66f95230
mariia mishchenko <mariiamishchenko@mariias-MacBook-Air.local>
2015-04-16 17:38:22 -0400

    title

--- work.js
+++ work.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
-//  console.log("my js is loaded");
-    $('.hide-title img').mouseenter(function() {
-        $(this).siblings().animate ({
-            opacity:'1',
-            right: '50px'
-        }, {
-            duration: 1000
+$(document).ready(function () {
+            console.log("my js is loaded");
+            $('.hide-title img').mouseenter(function () {
+                $(this).siblings().animate({
+                    opacity: '1',
+                    right: '50px'
+                }, {
+                    duration: 1000
+                });
             });
-    });
-    $('.hide-title img').mouseleave(function(){
-        $(this).siblings().animate ({
-            opacity:'0',
-            right: '0'
-        }, {
-            duration: 1000
-    });
+            $('.hide-title img').mouseleave(function () {
+                $(this).siblings().animate({
+                    opacity: '0',
+                    right: '0'
+                }, {
+                    duration: 1000
+                });
+            });
 });
