4ac2c737de7b31ba6f14a1dfcb5e4e4d2b416614
mariia mishchenko <mariiamishchenko@mariias-MacBook-Air.local>
2015-04-22 08:46:30 -0400

    about page

---
+++ landing.js
@@ -0,0 +1,17 @@
+console.log("my js is loaded");
+function setup() {
+    var myCanvas = createCanvas(window.innerHeight, window.innerWidth);
+    myCanvas.parent('landing');
+    stroke(224,35,0);
+    strokeWeight(1);
+    noFill();
+}
+
+function draw() {
+}
+
+function mouseClicked() {
+    ellipse(mouseX, mouseY, 80, 80);
+       
+
+}
